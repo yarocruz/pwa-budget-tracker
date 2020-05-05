e41408979b32c798d5b031317bb527006a03efaa
yarocruz <yarocruz@gmail.com>
2020-05-05 19:33:05 -0400

    setup server.js to deploy to heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = proccess.env.PORT || 3000;
 
 const app = express();
 
@@ -15,7 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+
+mongoose.connect(proccess.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
